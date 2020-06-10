cacfb397850c356c92eb7f0212e5d02c59644f18
AMWOLLO VICTOR <amwollo@yahoo.com>
2020-06-11 02:26:35 +0300

    Fix login,signup,category and home links

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -74,13 +74,15 @@ return [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
-                // '/' => '/site/index',
+                '/' => '/site/index',
                 '/explore/index' => '/explore/photos',
                 // '<alias:\w+>/<id:\d+>' => 'site/<alias>',
                 'users/<id:[\x{4e00}-\x{9fa5}a-zA-Z0-9_]*>' => 'user/view',
                 // 'p/<id:\d+>' => 'user/view/view-post',
-                'explore/<category_id:\d+>' => 'explore/category',
+                'explore/<category_name:\w+>/<category_id:\d+>' => 'explore/category',
                 'posts/<post:\w+>' => 'post/view',
+                'login' => 'site/login',
+                'signup' => 'site/signup',
             ],
         ],
 
